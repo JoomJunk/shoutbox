9ffec2b75c8eec5ae48f4dfe9025c01589ad0887
Charlie <lodder15@hotmail.co.uk>
2013-01-25 17:51:56 +0000

    Update mod_shoutbox/script.php

--- mod_shoutbox/script.php
+++ mod_shoutbox/script.php
@@ -1,5 +1,11 @@
 <?php
-// No direct access to this file
+/**
+* @version   $Id:helper.php 2012-01-16 21:00:00
+* @package   JJ Shoutbox
+* @copyright Copyright (C) 2011 - 2012 JoomJunk. All rights reserved.
+* @license   http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
 defined('_JEXEC') or die('Restricted access');
  
 class mod_shoutboxInstallerScript
