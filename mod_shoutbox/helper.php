faef182622dce84c24a2e732d4bc1809264ff8d8
Charlie <lodder15@hotmail.co.uk>
2013-01-26 10:43:35 +0000

    Update mod_shoutbox/helper.php

--- mod_shoutbox/helper.php
+++ mod_shoutbox/helper.php
@@ -224,21 +224,22 @@ class modShoutboxHelper {
 		$db->setQuery($query);
 		$db->query();
 	}
-	
+		
 	function deleteall($delete) {
 		$db	= JFactory::getDBO();
-		$query = $db->getQuery(true);
-		$query->delete()
-		->from('#__shoutbox');
-		$db->setQuery($query, 0, $delete);
-		$db->query();
-	}
-	//Joomla 2.5 coding standard but doesn't work	
+		$query = "DELETE FROM #__shoutbox LIMIT ".$delete."";
+		$db->setQuery($query);
+		$db->query(); 
+	   }
+	
+	//Joomla 2.5 coding standard but doesn't work
 	//function deleteall($delete) {
 	//	$db	= JFactory::getDBO();
-	//	$query = "DELETE FROM #__shoutbox LIMIT ".$delete."";
-	//	$db->setQuery($query);
-	//	$db->query(); 
-	//   }
+	//	$query = $db->getQuery(true);
+	//	$query->delete()
+	//	->from('#__shoutbox');
+	//	$db->setQuery($query, 0, $delete);
+	//	$db->query();
+	//  }
 }
 ?>
