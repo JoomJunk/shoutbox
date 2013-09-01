f181646a77ff880cd5b34059b4c2988251ef8f69
wilsonge <georgejameswilson@googlemail.com>
2013-09-01 02:20:16 +0100

    Fix issues with default colour form field

--- mod_shoutbox/script.php
+++ mod_shoutbox/script.php
@@ -1,6 +1,5 @@
 <?php
 /**
- * @version    $Id: script.php 2012-01-16 21:00:00
  * @package    JJ_Shoutbox
  * @copyright  Copyright (C) 2011 - 2013 JoomJunk. All rights reserved.
  * @license    GPL v3.0 or later http://www.gnu.org/licenses/gpl-3.0.html
@@ -8,8 +7,48 @@
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
+/**
+ * Shoutbox installation script class.
+ *
+ * @since  1.1.2
+ */
 class Mod_ShoutboxInstallerScript
 {
+	/**
+	 * @var		string	The version number of the module.
+	 * @since   1.2.4
+	 */
+	protected $release = '';
+
+	/**
+	 * @var		string	The table the parameters are stored in.
+	 * @since   1.2.4
+	 */
+	protected $paramTable = '#__modules';
+
+	/**
+	 * @var		string	The table column the parameters are stored in.
+	 * @since   1.2.4
+	 */
+	protected $paramColumn = 'params';
+
+	/**
+	 * @var		string	The extension name.
+	 * @since   1.2.4
+	 */
+	protected $extension = 'mod_shoutbox';
+
+	/**
+	 * Function called before module installation/update/removal procedure commences
+	 *
+	 * @param   string                   $type    The type of change (install, update or discover_install
+	 *                                            , not uninstall)
+	 * @param   JInstallerAdapterModule  $parent  The class calling this method
+	 *
+	 * @return  mixed   void on success and false on failure
+	 *
+	 * @since  1.1.2
+	 */
 	public function preflight($type, $parent)
 	{
 		// Module manifest file version
@@ -18,7 +57,9 @@ class Mod_ShoutboxInstallerScript
 		// Abort if the module being installed is not newer than the currently installed version
 		if ($type == 'Update')
 		{
-			$oldRelease = $this->getParam('version');
+			$manifest = $this->getItemArray('manifest_cache', '#__extensions', 'element', $this->extension);
+			$oldRelease = $manifest['version'];
+
 			$rel = $oldRelease . JText::_('MOD_SHOUTBOX_VERSION_TO') . $this->release;
 
 			if (version_compare($this->release, $oldRelease, 'lt'))
@@ -37,55 +78,279 @@ class Mod_ShoutboxInstallerScript
 
 			if (version_compare($oldRelease, $this->release, '<'))
 			{
+				// Update db schema to reflect new change in version 1.1.4
 				if (version_compare($oldRelease, '1.1.3', '<='))
 				{
-					$db = JFactory::getDBO();
-					$sql = "ALTER TABLE #__shoutbox ADD COLUMN user_id int(11) NOT NULL DEFAULT '0'";
-					$db->setQuery($sql);
-					$db->query();
+					$this->update114();
 				}
 
+				/**
+				 * For extensions going from < version 1.2.4 rename colour form field values and move assets
+				 * folder to media folder
+				 */
 				if (version_compare($oldRelease, '1.2.3', '<='))
 				{
-					if (JFolder::create('media/mod_shoutbox')
-						&& JFolder::move(JUri::root() . 'modules/mod_shoutbox/assets/css', JUri::root() . 'media/mod_shoutbox')
-						&& JFolder::move(JUri::root() . 'modules/mod_shoutbox/assets/images', JUri::root() . 'media/mod_shoutbox')
-						&& JFolder::move(JUri::root() . 'modules/mod_shoutbox/assets/js', JUri::root() . 'media/mod_shoutbox')
-						&& JFolder::move(JUri::root() . 'modules/mod_shoutbox/assets/recaptcha', JUri::root() . 'media/mod_shoutbox')
-						&& JFile::move(JUri::root() . 'modules/mod_shoutbox/assets/index.html', JUri::root() . 'media/mod_shoutbox/index.html'))
-					{
-						JFolder::delete(JPATH_ROOT . '/modules/mod_shoutbox/assets');
-					}
+					$this->update124();
 				}
 			}
 		}
-		else
-		{
-			$rel = $this->release;
-		}
 	}
 
-	public function install( $parent )
+	/**
+	 * Function called on install of module
+	 *
+	 * @param   JInstallerAdapterModule  $parent  The class calling this method
+	 *
+	 * @return  void
+	 *
+	 * @since  1.1.2
+	 */
+	public function install($parent)
 	{
 		echo '<p>' . JText::_('MOD_SHOUTBOX_INSTALL') . '</p>';
 	}
 
-	public function update( $parent )
+	/**
+	 * Function called on update of module
+	 *
+	 * @param   JInstallerAdapterModule  $parent  The class calling this method
+	 *
+	 * @return  void
+	 *
+	 * @since 1.1.2
+	 */
+	public function update($parent)
 	{
 		echo '<p>' . JText::_('MOD_SHOUTBOX_UPDATE') . $this->release . '</p>';
 		echo '<p>' . JText::_('MOD_SHOUTBOX_UPDATE_CHANGELOG') . '</p>';
 	}
 
-	public function getParam($name)
+	/**
+	 * Gets each instance of a module in the #__modules table
+	 * For all other extensions see alternate query
+	 *
+	 * @return  array  An array of ID's of the extension
+	 *
+	 * @since  1.2.4
+	 * @see getExtensionInstance
+	 */
+	protected function getModuleInstances()
 	{
 		$db = JFactory::getDbo();
 		$query = $db->getQuery(true);
-		$query->select('manifest_cache')
-			->from('#__extensions')
-			->where('name =' . $db->Quote('JJ Shoutbox'));
-		$db->setQuery($query);
-        $manifest = json_decode( $db->loadResult(), true );
 
-		return $manifest[$name];
+		// Select the item(s) and retrieve the id
+		$query->select($db->quoteName('id'))
+			->from($db->quoteName('#__modules'))
+			->where('module = ' . $db->Quote($this->extension));
+
+		// Set the query and obtain an array of id's
+		$db->setQuery($query);
+		$items = $db->loadColumn();
+
+		return $items;
+	}
+
+	/**
+	 * Gets each instance of any extension that isn't a module
+	 *
+	 * @return  string  The id of an extension in the #__extension table
+	 *
+	 * @since  1.2.4
+	 * @see    getModuleInstances
+	 */
+	protected function getExtensionInstance()
+	{
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		// Select the item and retrieve the id
+		$query->select($db->quoteName('id'))
+			->from($db->quoteName('#__extensions'))
+			->where('element = ' . $db->Quote($this->extension));
+
+		// Set the query and obtain the extension id
+		$db->setQuery($query);
+		$item = $db->loadResult();
+
+		return $item;
+	}
+
+	/**
+	 * Gets parameter value in the extensions row of the extension table
+	 *
+	 * @param   string   $name  The name of the parameter to be retrieved
+	 * @param   integer  $id    The id of the item in the Param Table
+	 *
+	 * @return  string  The parameter desired
+	 *
+	 * @since 1.2.4
+	 */
+	protected function getParam($name, $id = 0)
+	{
+		if (!is_int($id) || $id == 0)
+		{
+			// Return false if there is no item given
+			return false;
+		}
+
+		$params = $this->getItemArray($this->paramColumn, $this->paramTable, 'id', $id);
+
+		return $params[$name];
+	}
+
+	/**
+	 * Sets parameter values in the extensions row of the extension table. Note that the
+	 * this must be called separately for deleting and editing. Note if edit is called as a
+	 * type then if the param doesn't exist it will be created
+	 *
+	 * @param   array    $param_array  The array of parameters to be added/edited/removed
+	 * @param   string   $type         The type of change to be made to the param (edit/remove)
+	 * @param   integer  $id           The id of the item in the relevant table
+	 *
+	 * @return  mixed  false on failure, void otherwise
+	 */
+	protected function setParams($param_array = null, $type = 'edit', $id = 0)
+	{
+		if (!is_int($id) || $id == 0)
+		{
+			// Return false if there is no valid item given
+			return false;
+		}
+
+		$params = $this->getItemArray($this->paramColumn, $this->paramTable, 'id', $id);
+
+		if ($param_array)
+		{
+			foreach ($param_array as $name => $value)
+			{
+				if ($type == 'edit')
+				{
+					// Add or edit the new variable(s) to the existing params
+					$params[(string) $name] = (string) $value;
+				}
+				elseif ($type == 'remove')
+				{
+					// Unset the parameter from the array
+					unset($params[(string) $name]);
+				}
+			}
+		}
+
+		// Store the combined new and existing values back as a JSON string
+		$paramsString = json_encode($params);
+
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+		$query->update($db->quoteName($this->paramTable))
+			->set($this->paramColumn . ' = ' . $db->quote($paramsString))
+			->where('id = ' . $id);
+
+		// Update table
+		$db->setQuery($query);
+
+		if (version_compare(JVERSION, '3.0.0', 'ge'))
+		{
+			$db->execute();
+		}
+		else
+		{
+			$db->query();
+		}
+	}
+
+	/**
+	 * Builds a standard select query to produce better DRY code in this script.
+	 * This should produce a single unique cell which is json encoded
+	 *
+	 * @param   string  $element     The element to get from the query
+	 * @param   string  $table       The table to search for the data in
+	 * @param   string  $column      The column of the database to search from
+	 * @param   string  $identifier  The property of the column to search for
+	 *
+	 * @return  array  associated array containing data from the cell
+	 *
+	 * @since 1.2.4
+	 */
+	protected function getItemArray($element, $table, $column, $identifier)
+	{
+		// Get the DB and query objects
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		// Build the query
+		$query->select($db->quoteName($element))
+			->from($db->quoteName($table))
+			->where($column . ' = ' . $db->Quote($identifier));
+		$db->setQuery($query);
+
+		// Load the single cell and json_decode data
+		$array = json_decode($db->loadResult(), true);
+
+		return $array;
+	}
+
+	/**
+	 * Function to update the db schema for the Shoutbox Version 1.1.4 Updates
+	 *
+	 * @return  void
+	 *
+	 * @since  1.2.4
+	 */
+	protected function update114()
+	{
+		/*
+		 * Using SQL Statement here as JJ Shoutbox didn't support non-mysql databases at this point
+		 * and Joomla doesn't support changing the db schema
+		 */
+		$db = JFactory::getDbo();
+		$sql = "ALTER TABLE #__shoutbox ADD COLUMN user_id int(11) NOT NULL DEFAULT '0'";
+		$db->setQuery($sql);
+		$db->query();
+	}
+
+	/**
+	 * Function to update the file structure and params for the Shoutbox Version 1.2.4 updates
+	 *
+	 * @return  void
+	 *
+	 * @since  1.2.4
+	 */
+	protected function update124()
+	{
+		// Move the assets
+		if (JFolder::create('media/mod_shoutbox')
+			&& JFolder::move(JUri::root() . 'modules/mod_shoutbox/assets/css', JUri::root() . 'media/mod_shoutbox')
+			&& JFolder::move(JUri::root() . 'modules/mod_shoutbox/assets/images', JUri::root() . 'media/mod_shoutbox')
+			&& JFolder::move(JUri::root() . 'modules/mod_shoutbox/assets/js', JUri::root() . 'media/mod_shoutbox')
+			&& JFolder::move(JUri::root() . 'modules/mod_shoutbox/assets/recaptcha', JUri::root() . 'media/mod_shoutbox')
+			&& JFile::move(JUri::root() . 'modules/mod_shoutbox/assets/index.html', JUri::root() . 'media/mod_shoutbox/index.html'))
+		{
+			JFolder::delete(JPATH_ROOT . '/modules/mod_shoutbox/assets');
+		}
+
+		/*
+		 * We have moved to use the colour form field so a hash must be applied
+		 * to the parameters for them to function as expected still.
+		 */
+		$modules = $this->getModuleInstances();
+
+		foreach ($modules as $module)
+		{
+			// Convert string to integer
+			$module = (int) $module;
+
+			// Create array of params to change
+			$colours = array();
+			$colours['deletecolor'] = '#' . $this->getParam('deletecolor', $module);
+			$colours['headercolor'] = '#' . $this->getParam('headercolor', $module);
+			$colours['bordercolor'] = '#' . $this->getParam('bordercolor', $module);
+
+			// Set the param values
+			$this->setParams($colours, 'edit', $module);
+
+			// Unset the array for the next loop
+			unset($colours);
+		}
 	}
 }
