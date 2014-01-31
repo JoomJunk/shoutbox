66a016b519ac384df7532f331b6fa153322d1fc5
Charlie <lodder15@hotmail.co.uk>
2014-01-31 23:00:02 +0000

    Updated jQuery 1.11.0

--- mod_shoutbox/mod_shoutbox.php
+++ mod_shoutbox/mod_shoutbox.php
@@ -38,7 +38,7 @@ if ($smile == 1 || $smile == 2)
 		if (!JFactory::getApplication()->get('jquery'))
 		{
 			JFactory::getApplication()->set('jquery', true);
-			JHtml::_('script', 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+			JHtml::_('script', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js');
 			JHtml::_('script', JUri::root() . 'media/mod_shoutbox/js/jquery-conflict.js'); 
 		}
 	}
