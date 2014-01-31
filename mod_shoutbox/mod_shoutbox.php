dba954936269a496e3ee9910d735da8e46497505
George Wilson <georgejameswilson@googlemail.com>
2014-01-31 00:45:52 +0000

    Add back no conflict file

--- mod_shoutbox/mod_shoutbox.php
+++ mod_shoutbox/mod_shoutbox.php
@@ -40,7 +40,6 @@ if ($smile == 1 || $smile == 2)
 			JFactory::getApplication()->set('jquery', true);
 			JHtml::_('script', 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
 			JHtml::_('script', JUri::root() . 'media/mod_shoutbox/js/jquery-conflict.js'); 
-			// TODO - Upload noConflict file again when back at home
 		}
 	}
 }
