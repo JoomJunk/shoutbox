fd6961ef3408a8325711ebb81bc5a3f176578a20
Lodder <lodder15@hotmail.co.uk>
2014-08-24 22:44:58 +0100

    fixed Kunena profile link

--- mod_shoutbox/helper.php
+++ mod_shoutbox/helper.php
@@ -325,7 +325,14 @@ class ModShoutboxHelper
 			elseif ($profile == 2)
 			{
 				// Kunena Profile Link
-				$profile_link = '<a href="' . JRoute::_('index.php?option=com_kunena&view=user&userid=' . $user_id) . '">' . $name . '</a>';
+				if (class_exists('KunenaFactory') && class_exists('KunenaProfileKunena')) {
+					$kUser = KunenaFactory::getUser()->userid;
+					$kLink = KunenaProfileKunena::getProfileURL($kUser);
+				}
+				else {
+					$kLink = null;
+				}
+				$profile_link = '<a href="' . $kLink . '">' . $name . '</a>';
 			}
 			elseif ($profile == 3)
 			{
