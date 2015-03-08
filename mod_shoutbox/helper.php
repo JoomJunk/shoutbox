5230043b6edd882b7bcdb023e31b40264a4525f3
wilsonge <georgejameswilson@googlemail.com>
2015-03-08 12:34:09 +0000

    [security] Fix XSS vunerability

--- mod_shoutbox/helper.php
+++ mod_shoutbox/helper.php
@@ -388,9 +388,14 @@ class ModShoutboxHelper
 
 		$ip = $_SERVER['REMOTE_ADDR'];
 
+		// Sanity check on the contents of the user fields
+		$filter = JFilterInput::getInstance();
+		$name = $filter->clean($name, 'string');
+		$message = $filter->clean($message, 'string');
+
 		if ($swearCounter == 0 || $swearCounter == 1 && (($nameSwears + $messageSwears) <= $swearNumber))
 		{
-			return $this->addShout(htmlspecialchars($name), htmlspecialchars($message), $ip);
+			return $this->addShout($name, $message, $ip);
 		}
 	}
 
