ebb6b9777097ddc189a85fa2576063c62eebecab
George Wilson <georgejameswilson@googlemail.com>
2015-05-26 00:42:33 +0100

    Update changelog

--- changelog.php
+++ changelog.php
@@ -18,6 +18,7 @@ WIP
 + Added German translation (thanks Betteryouthanme)
 ^ Updates to Polish language files (thanks PLFoxNET)
 # Fixed update script for J3.4+
+# Fixed issue with message length in UTF-8 languages
 
 Version 4.0.2
 # Fixed PHP error when no menu item ID exists
