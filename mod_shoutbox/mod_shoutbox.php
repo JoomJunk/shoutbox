d77a7ce8692f6afc57596b3b6634397fe6157d66
George Wilson <georgejameswilson@googlemail.com>
2015-06-10 20:47:13 +0100

    Update jQuery version

--- mod_shoutbox/mod_shoutbox.php
+++ mod_shoutbox/mod_shoutbox.php
@@ -85,7 +85,7 @@ else
 	if (!$app->get('jquery'))
 	{
 		$app->set('jquery', true);
-		$doc->addScript('//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js');
+		$doc->addScript('//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js');
 		JHtml::_('script', 'mod_shoutbox/jquery-conflict.js', false, true);
 	}
 }
