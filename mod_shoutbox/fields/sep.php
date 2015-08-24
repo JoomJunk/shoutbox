5f7d6683012f0a3497e9d5b2eee2b105714ecd90
C-Lodder <lodder15@hotmail.co.uk>
2015-08-24 11:45:21 +0100

    Categorised advanced parameters for ease of view

---
+++ mod_shoutbox/fields/sep.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* @package    JJ_Shoutbox
+* @copyright  Copyright (C) 2011 - 2015 JoomJunk. All rights reserved.
+* @license    GPL v3.0 or later http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+defined('JPATH_PLATFORM') or die;
+/**
+ * Form Field separator for JoomJunk.
+ *
+ * @package     JJ_Shoutbox
+ * @since       5.0.2
+ */
+class JFormFieldSep extends JFormField
+{
+	/**
+	 * @var string
+	 */
+	protected $type = 'Sep';
+	/**
+	 * @return string
+	 */
+	protected function getLabel()
+	{
+        $doc = JFactory::getDocument();
+        $doc->addStyleDeclaration('.jj-sep { border-bottom:1px solid #eee;font-size:16px;color:#BD362F;margin-top:15px;padding:2px 0;width:100% }');
+		
+        $label = JText::_((string)$this->element['label']);
+        $css   = (string)$this->element['class'];
+		
+        return '<div class="jj-sep ' . $css . '">' . $label . '</div>';
+	}
+	/**
+	 * @return mixed
+	 */
+	protected function getInput()
+	{
+        return;
+	}
+}
