122ea392c2bf82dd9b938f685ef178c8978c2766
C-Lodder <lodder15@hotmail.co.uk>
2015-09-14 09:41:17 +0100

    Updated changelog WIP

--- changelog.php
+++ changelog.php
@@ -16,6 +16,8 @@ $ -> Language fix or change
 
 WIP
 + Added option to hide security question for registered users
++ Added HTML5 notifications
++ Added option to hide security question for registered users
 ^ Moved swear words to repeatable form fields
 ^ Updated ReCaptcha to v2
 $ Some language strings changed for ReCaptcha v2
