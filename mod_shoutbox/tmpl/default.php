aa57e53dacc46e62d28e9181e4cb210563cf331d
Lodder <lodder15@hotmail.co.uk>
2016-01-23 15:55:02 +0000

    Prevent ReCaptcha executing from 3rd party sources

--- mod_shoutbox/tmpl/default.php
+++ mod_shoutbox/tmpl/default.php
@@ -500,7 +500,10 @@ JText::script('SHOUT_AJAX_ERROR');
 			}			
 			else
 			{
-				var JJ_Recaptcha = typeof(grecaptcha) == 'undefined' ? '' : grecaptcha.getResponse();
+				var JJ_Recaptcha = '';
+				<?php if ($securitytype == 1) : ?>
+				JJ_Recaptcha = typeof(grecaptcha) == 'undefined' ? '' : grecaptcha.getResponse();
+				<?php endif; ?>
 
 				JJShoutbox.submitPost(shoutId, type, name, '<?php echo $title; ?>', <?php echo $securitytype; ?>, '<?php echo JSession::getFormToken(); ?>', Itemid, instance, JJ_Recaptcha, JJ_History);
 			}
