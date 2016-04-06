5a15c1e63d0d8cbf7696efe5c7133dc27138447e
George Wilson <georgejameswilson@googlemail.com>
2016-04-06 22:29:05 +0100

    Notifications should be an integer

--- mod_shoutbox/tmpl/default.php
+++ mod_shoutbox/tmpl/default.php
@@ -471,7 +471,7 @@ JText::script('SHOUT_AJAX_ERROR');
 		setInterval(function(){
 			var JJ_itemId = '<?php echo $Itemid; ?>';
 			var JJ_insertName = '<?php echo $displayName == 'user' ? $user->username : $user->name; ?>';
-			JJShoutbox.getPosts('<?php echo $title; ?>', '<?php echo $sound; ?>', '<?php echo $notifications; ?>', JJ_itemId, JJ_instance, JJ_insertName, JJ_history);
+			JJShoutbox.getPosts('<?php echo $title; ?>', '<?php echo $sound; ?>', <?php echo $notifications; ?>, JJ_itemId, JJ_instance, JJ_insertName, JJ_history);
 		}, <?php echo $refresh; ?>);
 		<?php endif; ?>
 	});	
