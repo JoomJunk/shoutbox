cdb16d4b433a4ecbb52cc7b870df1495ced7412c
C-Lodder <lodder15@hotmail.co.uk>
2016-06-06 16:52:48 +0100

    Add Pro version message

---
+++ mod_shoutbox/fields/pro.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package    JJ_Shoutbox
+ * @author     JoomJunk <admin@joomjunk.co.uk>
+ * @copyright  Copyright (C) 2011 - 2016 JoomJunk. All Rights Reserved
+ * @license    GPL v3.0 or later http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Notification for Pro version.
+ *
+ * @package     JJ_Shoutbox
+ * @since       1.5.6
+ */
+class JFormFieldPro extends JFormField
+{
+	/**
+	 * @var string
+	 */
+	protected $type = 'Pro';
+
+	/**
+	 * @return string
+	 */
+	protected function getLabel()
+	{
+		$app = JFactory::getApplication();
+		$msg = '<h3>Love JJ Shoutbox? Take a look at the <a href="http://joomjunk.co.uk/products/shoutbox-pro.html" target="_blank">Pro version</a> which is packed with many more features.</h3>';
+
+        return $app->enqueueMessage($msg, 'message');
+	}
+
+	/**
+	 * @return mixed
+	 */
+	protected function getInput()
+	{
+        return;
+	}
+}
